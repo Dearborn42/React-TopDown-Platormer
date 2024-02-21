c4c4afff0649134ad28309408393b39b48d13e15
Dearborn42 <amurf26@outlook.com>
2024-02-21 15:18:24 -0700

    Working on shooting mechanics

--- client/src/P5Sketch.js
+++ client/src/P5Sketch.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import { ReactP5Wrapper } from "@p5-wrapper/react";
 
 
@@ -12,6 +12,8 @@ export default function P5Sketch() {
 
     const numEnemies = 5;
     let enemies = [];
+
+    var projectiles = [];
 
     function moveEnemy(enemy){
       let dx = playerX - enemy.x;
@@ -27,10 +29,41 @@ export default function P5Sketch() {
       p5.square(enemy.x, enemy.y, 50);
     };
 
+    p5.mouseClicked = () => {
+      const newProjectile = {
+        damage: 50,
+        x: playerX,
+        y: playerY,
+        speed: 7,
+        angle: p5.atan2(p5.mouseY - playerY, p5.mouseX - playerX),
+        color: p5.color(0, 255, 0)
+      };
+      projectiles.push(newProjectile);
+    };
+
+    const moveProjectile = (projectile) => {
+      let vx = p5.cos(projectile.angle) * projectile.speed;
+      let vy = p5.sin(projectile.angle) * projectile.speed;
+      projectile.x += vx;
+      projectile.y += vy;
+      if(projectile.x < 0 || projectile.x > window.innerWidth) {
+        projectiles = projectiles.filter((pro) => pro !== projectile)
+      }
+      if(projectile.y < 0 || projectile.y > window.innerHeight) {
+        projectiles = projectiles.filter((pro) => pro !== projectile)
+      }
+    };
+
+    const drawProjectile = (projectile) => {
+      p5.fill(projectile.color);
+      p5.rect(projectile.x, projectile.y, 10, 5);
+    };
+
     p5.setup = () => {
       p5.createCanvas(window.innerWidth, window.innerHeight);
       for (let i = 0; i < numEnemies; i++) {
         enemies.push({
+          health: 100,
           x: p5.random(0, p5.width),
           y: p5.random(0, p5.height),
           speed: p5.random(1, 4),
@@ -51,7 +84,10 @@ export default function P5Sketch() {
           drawEnemy(enemy);
         });
          
-        
+        projectiles.forEach(projectile => {
+          moveProjectile(projectile);
+          drawProjectile(projectile);
+        });
 
         if (p5.keyIsDown(65)) playerX -= playerSpeed;
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.css";
-import P5ConentWrapper from './P5ConentWrapper';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
