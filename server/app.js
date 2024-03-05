5bb779f9546e09bac3fa4635f24eb89626eeeef4
Sarc4sticNerd <jplam904@gmail.com>
2024-03-04 21:15:06 -0700

    uhh yeah

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
 import bodyParser from 'body-parser';
-import cors from 'cors'
+import cors from 'cors';
 import express from 'express';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import passport from 'passport';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import leaderboard_route from "./routes/leaderboard-route.js"
+import leaderboard_route from './routes/leaderboard-route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO_URI);
 
@@ -16,13 +16,14 @@ app.use(passport.initialize());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: false, limit: 100000, parameterLimit: 20}))
+app.use(
+  express.urlencoded({ extended: false, limit: 100000, parameterLimit: 20 })
+);
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-app.use("/leaderboard", leaderboard_route);
+app.use('/leaderboard', leaderboard_route);
 
-app.route('/').get((req, res) =>{
-    res.send("Welcome");
-})
-
+app.route('/').get((req, res) => {
+  res.send('Welcome');
+});
 
 app.listen(process.env.PORT);
