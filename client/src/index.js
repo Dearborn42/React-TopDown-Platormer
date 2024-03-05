c5370a48df946353a6425d86e7da92c03c2e8635
Sarc4sticNerd <jplam904@gmail.com>
2024-03-04 22:45:57 -0700

    leader

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "bootstrap/dist/css/bootstrap.css";
-
+import Leader from './Leader';
+import 'bootstrap/dist/css/bootstrap.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: '/game',
+    element: <App />,
+  },
+  {
+    path: '/',
+    element: <Leader />,
+  },
+]);
 root.render(
   // <React.StrictMode>
-      <App />
+  <RouterProvider router={router} />
   // </React.StrictMode>
 );
