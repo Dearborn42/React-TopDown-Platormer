cf04e52a6f320b0b59dc2e0b4b482f34508b2c8f
Dearborn42 <amurf26@outlook.com>
2024-03-05 19:02:12 -0700

    Update leaderboard-controller.js

--- server/controllers/leaderboard-controller.js
+++ server/controllers/leaderboard-controller.js
@@ -14,15 +14,30 @@ export async function editLeaderboard(req, res) {
     const { score, name } = req.body;
     const leaderboard = await Leaderboard.findById('65cd8b1fb471458aeb440eda');
     var keys = ['first', 'second', 'third', 'fourth', 'fifth'];
+    let newPosition = -1; // Indicates the position where the player should be inserted
     for (let i = 0; i < keys.length; i++) {
       const { playerScore } = leaderboard[keys[i]];
       if (score > playerScore) {
-        leaderboard[keys[i]].playerName = name;
-        leaderboard[keys[i]].playerScore = score;
-        await leaderboard.save();
-        return res.status(200).json({ success: true, leaderboard });
+        newPosition = i;
+        break;
       }
     }
+    if (newPosition !== -1) {
+      // Move lower-ranked players down
+      for (let i = keys.length - 1; i > newPosition; i--) {
+        leaderboard[keys[i]].playerName = leaderboard[keys[i - 1]].playerName;
+        leaderboard[keys[i]].playerScore = leaderboard[keys[i - 1]].playerScore;
+      }
+      // Insert the new player at the newPosition
+      leaderboard[keys[newPosition]].playerName = name;
+      leaderboard[keys[newPosition]].playerScore = score;
+    }
+
+    // Remove players below top 5
+    for (let i = keys.length - 1; i > 3; i--) {
+      delete leaderboard[keys[i]];
+    }
+    await leaderboard.save();
     return res.status(200).json({ success: true });
   } catch (error) {
     return res.status(505).json({ success: false, error: error.message });
